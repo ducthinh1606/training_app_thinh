fda30fe551ca1bdcf5a92c3f99bbdd4db2bad368
ducthinh1606 <emlaga98hp1@gmail.com>
2022-12-26 17:12:49 +0700

    feature Login UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import "./App.scss"
 import Header from "./components/Layout/Header"
 import ListTask from "./components/ListTask/ListTask";
 import AddTask from "./components/Task/AddTask";
+import Login from "./components/Auth/Login/Login";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <AddTask />
-        <ListTask />
+        <Login />
+        {/*<Header />*/}
+        {/*<AddTask />*/}
+        {/*<ListTask />*/}
     </div>
   );
 }
