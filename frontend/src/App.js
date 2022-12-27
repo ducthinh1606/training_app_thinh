b48e57e48e6b6ec20b5fd44a9906b3b1c6aee0d4
ducthinh1606 <emlaga98hp1@gmail.com>
2022-12-27 15:31:55 +0700

    feature Form main App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import Header from "./components/Layout/Header"
+import ListTask from "./components/ListTask/ListTask";
+import AddTask from "./components/Task/AddTask";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
-      </header>
+        <Header />
+        <AddTask />
+        <ListTask />
     </div>
   );
 }
---
+++ frontend/src/components/ListTaskByStatus/ListTaskByStatus.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./ListTaskByStatus.scss"
+import Task from "../Task/Task";
+
+function ListTaskByStatus({taskStatus}) {
+    const demoListTask = [
+        {
+            title: "1",
+            estimate: "2022-27-12 00:00"
+        },
+        {
+            title: "2",
+            estimate: "2022-27-12 00:00"
+        },
+        {
+            title: "3",
+            estimate: "2022-27-12 00:00"
+        },
+        {
+            title: "4",
+            estimate: "2022-27-12 00:00"
+        },
+        {
+            title: "5",
+            estimate: "2022-27-12 00:00"
+        },
+    ]
+
+    const listTask = demoListTask.map((data) => <Task task={data}/>)
+
+    return (
+        <div className="list-task-by-status">
+            <div className="status">{taskStatus.name}</div>
+            {listTask}
+        </div>
+    )
+}
+
+export default ListTaskByStatus;
